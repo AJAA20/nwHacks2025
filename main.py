af126b9ae05c5566de1e9b2132593cf753bfe04c
rjathan7 <rjathan1602@gmail.com>
2025-01-18 14:38:35 -0800

    connected fe to be

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ app = FastAPI() # Creates an instance of FastAPI, assigning it to the variable `
 
 '''
 This object manages the template loading and rendering process. By passing
-"templates" to the directory argument, we're telling FastAPI that your
-HTML files (like login.html) are located in a folder called templates to where
+"templates" to the directory argument, we're telling FastAPI that the
+HTML files (like test.html) are located in a folder called templates to where
 app.py is located
 '''
 templates = Jinja2Templates(directory="templates") 
 
-@app.get("/")
-def read_root():
-    return "nwHacks2025"
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "hello": q}
+# decorator that binds read_root to GET request to the root
+# when a user accesses http://localhost:8000/, this function is triggered 
+# By default, FastAPI returns JSON responses, but since we're rendering an HTML page
+# we specify that the response should be of type HTMLResponse.
+# This tells FastAPI to send HTML content back to the client, rather than JSON.
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # "request":is passed so that it can be used by the template 
+    # (some templates may need access to request information, such as the user's IP address, etc)
+    return templates.TemplateResponse("test.html", {"request": request, "message": "nwHacks2025"})
 
 
